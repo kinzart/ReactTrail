362db573c358b2614e71cd370d6d1195660189e5
kinzart <kinzartmusica@gmail.com>
2021-06-22 19:32:45 -0300

    mudanças

--- src/components/Button1.tsx
+++ src/components/Button1.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 type Button1Props = {
     text?: string;//? = opcional, tem ou nao tem
     txtId: number;
 }
 
-
+const talNome = window.prompt('Digite seu nome')
 export function Button1(props: Button1Props) {
-    const talNome = window.prompt('Digite seu nome')
+
+    const [counter, setCounter] = useState(0);
+    // [var, adjustor da var] recebe estado 0
+    function increment() {
+        setCounter(counter - 1); //ajustar valor da var baseado no var inicial
+        console.log(counter);
+    }
     return (
+
+
         <div>
-            <button>{props.text || props.txtId}</button>
+            <button onClick={increment}>{props.text || props.txtId}{counter}</button>
             <h2>Aperte o botão acima {talNome}</h2>
         </div>
     )
